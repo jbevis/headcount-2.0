d6b528d52f5115ce149ec75dc4323654068d4676
Jack Bevis <jack.bevis@gmail.com>
2017-05-01 19:12:38 -0600

    findByName function returning undefined for non existant locations and object of location for ones that exist, tests for interation one part one passing

--- src/helper.js
+++ src/helper.js
@@ -1,16 +1,35 @@
 export default class DistrictRepository {
-  constructor(data){
+  constructor(data) {
     this.data = this.dataCleanFunction(data) || {}
   }
 
   dataCleanFunction(dataSet) {
     return dataSet.reduce((acc, val) => {
-        !acc[val.Location] ?
-          acc[val.Location] = {[val.TimeFrame]: val.Data} :
-          acc[val.Location][val.TimeFrame] = val.Data
+      // !acc[val.Location] ?
+      //     acc[val.Location] = {[val.TimeFrame]: val.Data} :
+      //     acc[val.Location][val.TimeFrame] = val.Data
+      let { Location, TimeFrame, Data } = val;
+      if (!acc[Location]) {
+        acc[Location] = {};
+        acc[Location].location = Location;
+        acc[Location].data = {};
+      }
 
-        return acc
-      }, {})
+      acc[val.Location].data[val.TimeFrame] = val.Data
 
+      return acc
+    }, {})
+  }
+
+  findByName(name) {
+   if (!name) {
+     return undefined;
+   }
+   let school = Object.keys(this.data).filter(key => {
+     if (name === key) {
+       return this.data[key]
+     }
+   })
+   return this.data[school]
   }
 }
